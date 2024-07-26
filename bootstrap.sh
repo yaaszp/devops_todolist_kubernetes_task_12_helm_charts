f86305d78803d1af3899275e4266f385bf3b1304
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-07-26 21:43:37 +0300

    needed changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 helm install todoapp .infrastructure/helm-chart/todoapp
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
